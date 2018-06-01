2a525be02226cffb3ad92a5f1eff7957112f4aca
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-06-01 16:24:57 +0300

    Initial commit

---
+++ src/main/java/xyz/vegaone/security/SecurityApplication.java
@@ -0,0 +1,12 @@
+package xyz.vegaone.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+}
